bc6cb3dd08258f495431558222ba96de01966ed5
asd <aravindhv10@gmail.com>
2025-07-13 12:25:29 +0530

    More work towards onnx inference

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
-use kornia::image::Image;
-use kornia::io::functional as F;
+use image::{GenericImageView, imageops, imageops::FilterType};
+use ndarray::{Array, Axis, s};
+use std::path::Path;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+use ort::{
+    inputs,
+    session::{Session, SessionOutputs},
+    value::TensorRef,
+};
+
+use raqote::{DrawOptions, DrawTarget, LineJoin, PathBuilder, SolidSource, Source, StrokeStyle};
+// use show_image::{AsImageView, WindowOptions, event};
+// use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+const CLASS_LABELS: [&str; 3] = ["empty", "occupied", "other"];
+
+fn main() {
+    let original_img = image::open(Path::new(
+        "/home/asd/DATASET/image_dataset/both_arms_out/v36frame0048.jpg",
+    ))
+    .unwrap();
+    let (width, height) = (original_img.width(), original_img.height());
+    let size = width.min(height);
+    let x = (width - size) / 2;
+    let y = (height - size) / 2;
+    let cropped_img = imageops::crop_imm(&original_img, x, y, size, size).to_image();
+
+    // let img = original_img.resize_exact(448, 448, FilterType::CatmullRom);
     // read the image
-    Ok(())
 }
