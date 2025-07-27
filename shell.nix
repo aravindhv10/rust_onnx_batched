c162472dedc53bec8e04a5c47df1c2a0d9e6f2c5
Your Name <you@example.com>
2025-07-27 19:15:36 +0530

    Added cuda

--- shell.nix
+++ shell.nix
@@ -1,32 +1,42 @@
-{pkgs ? import <nixpkgs> {}}: let
-  mylist = with pkgs; [
-    bc
-    bison
-    blend2d
-    cargo
-    cargo-info
-    ffmpeg
-    ffmpeg.dev
-    fish
-    flex
-    fontconfig
-    fontconfig.dev
-    fontconfig.lib
-    gnumake
-    libelf
-    nasm
-    openssl
-    openssl.dev
-    pkg-config
-    python313Full
-    udev
-    zsh
-    zstd
-  ];
-in (pkgs.mkShell {
-  name = "good_rust_env";
+{pkgs ? import <nixpkgs> {}} :
 
-  packages = mylist;
+let
 
-  runScript = "fish";
+mylist = with pkgs; [
+
+bc
+bison
+blend2d
+cargo
+cargo-info
+ffmpeg
+ffmpeg.dev
+fish
+flex
+fontconfig
+fontconfig.dev
+fontconfig.lib
+gnumake
+libelf
+nasm
+openssl
+openssl.dev
+pkg-config
+python313Full
+udev
+zsh
+zstd
+
+] ;
+
+in
+
+(pkgs.mkShell {
+
+name = "good_rust_env";
+
+packages = mylist;
+
+runScript = "fish";
+
 })
