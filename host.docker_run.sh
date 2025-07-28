8f46f296707beb2c2b0e9b6bb8a13957494dead5
Your Name <you@example.com>
2025-07-28 17:54:26 +0530

    More docker script updates

--- host.docker_run.sh
+++ host.docker_run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-A1="$(realpath "${1}")"
-A2="$(realpath "${2}")"
+A1="$(realpath .)"
 cd "$('dirname' -- "${0}")"
 cat './host.docker_run.txt' | tr '\n' ' ' > './host.docker_run_main.sh'
-sh './host.docker_run_main.sh' "${A1}" "${A2}"
+sh './host.docker_run_main.sh' "${A1}"
