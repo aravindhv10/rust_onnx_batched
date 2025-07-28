b218796b75876dfc327c8998aefa87185f680947
Your Name <you@example.com>
2025-07-28 17:50:38 +0530

    More fixes to infer file

--- infer.sh
+++ infer.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-curl -X POST "http://127.0.0.1:8000/infer" -F "image=@./image.png"
-curl -X POST "http://127.0.0.1:8000/infer" -F "image=@./image.jpg"
+curl -X POST "http://127.0.0.1:8000/infer" -F "file=@./image.png"
+curl -X POST "http://127.0.0.1:8000/infer" -F "file=@./image.jpg"
