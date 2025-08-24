d2d45f3bbff40feb55d13619fa013b25aaf8c092
asd <aravindhv10@gmail.com>
2025-08-24 10:41:46 +0530

    VERIFIED: More significant doc reorg

--- shell.nix
+++ shell.nix
@@ -1,42 +1,32 @@
-{pkgs ? import <nixpkgs> {}} :
+{pkgs ? import <nixpkgs> {}}: let
+  mylist = with pkgs; [
+    bc
+    bison
+    blend2d
+    cargo
+    cargo-info
+    ffmpeg
+    ffmpeg.dev
+    fish
+    flex
+    fontconfig
+    fontconfig.dev
+    fontconfig.lib
+    gnumake
+    libelf
+    nasm
+    openssl
+    openssl.dev
+    pkg-config
+    python313Full
+    udev
+    zsh
+    zstd
+  ];
+in (pkgs.mkShell {
+  name = "good_rust_env";
 
-let
+  packages = mylist;
 
-mylist = with pkgs; [
-
-bc
-bison
-blend2d
-cargo
-cargo-info
-ffmpeg
-ffmpeg.dev
-fish
-flex
-fontconfig
-fontconfig.dev
-fontconfig.lib
-gnumake
-libelf
-nasm
-openssl
-openssl.dev
-pkg-config
-python313Full
-udev
-zsh
-zstd
-
-] ;
-
-in
-
-(pkgs.mkShell {
-
-name = "good_rust_env";
-
-packages = mylist;
-
-runScript = "fish";
-
+  runScript = "fish";
 })
