9ff1ae301b4f971780f0ae1ec908da9123ddf683
asd <aravindhv10@gmail.com>
2025-09-08 22:37:45 +0530

    Added client to git

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd "$(dirname -- "${0}")"
 export RUSTFLAGS="-C target-cpu=native"
-cargo run --release --bin 'infer-server'
+# cargo run --release --bin 'infer-server'
+cargo run --release --bin 'infer-client'
