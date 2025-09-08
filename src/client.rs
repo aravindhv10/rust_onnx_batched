d5b5b6979f98f5b1f00083e91176360ffc056152
asd <aravindhv10@gmail.com>
2025-09-08 22:49:28 +0530

    Added client to git

--- src/client.rs
+++ src/client.rs
@@ -2,15 +2,13 @@ pub mod infer {
     tonic::include_proto!("infer"); // The string specified here must match the proto package name
 }
 
-use std::fs;
 use std::error::Error;
+use std::fs;
 
 #[actix_web::main]
 async fn main() -> Result<(), tonic::transport::Error> {
-    let  data =  fs::read("./image.png");
-    let img = infer::Image{
-        image_data: data
-    }
+    let data = fs::read("./image.png");
+    let img = infer::Image { image_data: data };
     let mut client = infer::infer_client::InferClient::connect("0.0.0.0:8001").await?;
     client.do_infer(img);
     return Ok(());
