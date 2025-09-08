5f4f9fe541b6fd7ad373fa70d42dc0f27fa9e71f
asd <aravindhv10@gmail.com>
2025-09-08 22:42:15 +0530

    Added client to git

--- src/client.rs
+++ src/client.rs
@@ -6,7 +6,7 @@ use std::error::Error;
 use std::fs;
 
 #[actix_web::main]
-async fn main() -> std::io::Result<(), tonic::transport::Error> {
+async fn main() -> Result<(), tonic::transport::Error> {
     let mut client = infer::infer_client::InferClient::connect("0.0.0.0:8001").await?;
     let data = fs::read("./image.png");
     return Ok(());
