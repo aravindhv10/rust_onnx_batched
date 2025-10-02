856220f7a1d07612303cf0753d48c7a64b07a771
Your Name <you@example.com>
2025-10-02 15:40:12 +0530

    More migraphx debugging

--- setup_rocm_2.sh
+++ setup_rocm_2.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
 
-sudo tee /etc/apt/sources.list.d/rocm.list << EOF
+tee /etc/apt/sources.list.d/rocm.list << EOF
 deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/rocm/apt/7.0.1 noble main
 deb [arch=amd64 signed-by=/etc/apt/keyrings/rocm.gpg] https://repo.radeon.com/graphics/7.0.1/ubuntu noble main
 EOF
 
-sudo tee /etc/apt/preferences.d/rocm-pin-600 << EOF
+tee /etc/apt/preferences.d/rocm-pin-600 << EOF
 Package: *
 Pin: release o=repo.radeon.com
 Pin-Priority: 600
 EOF
-
-sudo apt update
